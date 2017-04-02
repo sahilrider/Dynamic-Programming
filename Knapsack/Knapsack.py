f7a5a201b65e7b4f0d75344d1d6a95dcf25048b6
Sahil Kumar <sahilriders@gmail.com>
2017-04-03 02:19:36 +0530

    Memoized

--- Knapsack/Knapsack.py
+++ Knapsack/Knapsack.py
@@ -1,5 +1,5 @@
 memo={}
-parent=[]
+included={}
 def DP(val,s,i,x):
     if i==len(val) or x==0:
         return 0
@@ -8,25 +8,27 @@ def DP(val,s,i,x):
     #Not Possible to be added
     if s[i]>x:
         memo[(i,x)]=DP(val,s,i+1,x)
+        included[i]=0
         return memo[(i,x)]
     else:
         #Not Added
         if DP(val,s,i+1,x)>val[i]+DP(val,s,i+1,x-s[i]):
             memo[(i,x)]=DP(val,s,i+1,x)
+            included[i]=0
         #Added
         else:
             memo[(i,x)]=val[i]+DP(val,s,i+1,x-s[i])
-            if i not in parent:
-                parent.append(i)
+            included[i]=1
         return memo[(i,x)]
         
 
 '''Driver Program'''
-val=[4,2,2,1,10]
-s=[12,1,2,1,4]
+val=[4,10,2]
+s=[12,4,2]
 size=15
 value=DP(val,s,0,size)
 print('Maximum Value:',value)
 print('Subset:',end=' ')
-for i in sorted(parent):
-    print(val[i],end=' ')
+for i in included:
+    if included[i]:
+        print(val[i],end=' ')
